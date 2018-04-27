9769dca053c2a8bfa0d7468519a8f35346c41289
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-04-27 12:16:20 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Layout>
+					<BurgerBuilder />
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
